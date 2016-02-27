76b404b189cb868c3a6c84c5c9f79bc8a58cb5e5
Bnaya Peretz <bnaya@photomania.net>
2016-02-27 19:17:24 +0200

    jspm install & more

--- app/config.js
+++ app/config.js
@@ -19,18 +19,22 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.19",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
     "github:angular/bower-angular-animate@1.4.9": {
       "angular": "github:angular/bower-angular@1.4.9"
     },
     "github:angular/bower-angular-aria@1.4.9": {
       "angular": "github:angular/bower-angular@1.4.9"
     },
+    "github:angular/bower-angular-messages@1.5.0": {
+      "angular": "github:angular/bower-angular@1.5.0"
+    },
     "github:angular/bower-material@master": {
       "angular": "github:angular/bower-angular@1.4.9",
       "angular-animate": "github:angular/bower-angular-animate@1.4.9",
       "angular-aria": "github:angular/bower-angular-aria@1.4.9",
+      "angular-messages": "github:angular/bower-angular-messages@1.5.0",
       "css": "github:systemjs/plugin-css@0.1.18"
     }
   }
